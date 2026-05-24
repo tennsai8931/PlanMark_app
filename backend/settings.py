2993ccee4ba7d42cd40a26d7ef715f7ca3b0751a
Box CJ <tennsai9314@gmail.com>
2026-05-24 08:24:00 +0000

    Use environment variables for database settings

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'planmark'),
+        'USER': os.getenv('POSTGRES_USER', 'planmark_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'planmark_password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'host.docker.internal'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
